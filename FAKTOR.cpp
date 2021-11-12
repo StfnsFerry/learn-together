85d0819cf373312a298bcf96e28825480fe372c8
Stefanus Ferry Susanto <91201205+FerryJr@users.noreply.github.com>
2021-11-12 11:10:54 +0700

    Update FAKTOR.cpp

--- FAKTOR.cpp
+++ FAKTOR.cpp
@@ -8,24 +8,34 @@ int main (){
 	int Y = 0; 
 	int B = 0;
 	
-	cin>>T;;
+	cin>>T;
 	
-	int faktorr[T];
-
-	for (int i = 0; i < T ; i++){
-		cin >> x;
-		for(int j=x; j > 0;j--){
-			if(x%j == 0){
-				Y++;
+	if(T >= 1 && T <= 100){
+		
+		int faktorr[T];
+	
+		for (int i = 0; i < T ; i++){
+			cin >> x;
+			if(x >= 1 && x <= 1000000000){
+				for(int j=x; j > 0;j--){
+					if(x%j == 0){
+						Y++;
+					}
+				}
+				faktorr[i]=Y;
+				Y=0;
+			}
+			else{
+				exit(0);	
 			}
 		}
-		faktorr[i]=Y;
-		Y=0;
+		for (int i = 0; i < T ; i++){
+			cout << "Case #" << i+1 << " : " << faktorr[i]<<endl;
+		]
 	}
-	for (int i = 0; i < T ; i++){
-		cout << "Case #" << i+1 << " : " << faktorr[i]<<endl;
+	
+	else{
 	}
-
 	
 	return 0;
 }
